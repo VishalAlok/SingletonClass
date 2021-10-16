d8ed2d819c46fd3427aa228f0939992fe2aa0f3f
vishal.Alok <you@example.com>
2021-10-16 07:50:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Singleton
+{
+    public static Singleton obj = new Singleton();
+
+    public void Display()
+    {
+        Console.WriteLine(true);
+    }
+    Singleton() { } //Private So cannot create object outside
+}
+
+class SingletonDemo
+{
+    public static void Main()
+    {
+        //Singleton _obj = new Singleton(); //Wont Allow as constructor is private
+        Singleton.obj.Display(); //Calling static CLass Object
+    }
+}
